a3ec745963fcadee2e018db52b9bf37f45d9c602
Daniel Osvaldo Duda <daniel_duda93@yahoo.com.ar>
2024-08-17 12:25:43 -0300

    Update novedades.js

--- routes/novedades.js
+++ routes/novedades.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require('../models/novedadesModel');
+var cloudinary = require(`cloudinary`).v2;
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('novedades'); 
+
+router.get('/', async function(req, res, next) {
+
+  var novedades= await novedadesModel.getNovedades();
+
+
+  
+  novedades = novedades.map (novedad =>{
+
+    if(novedad.img_id){
+  
+        const imagen = cloudinary.url (novedad.img_id, {
+          
+  
+        });
+        return {
+          ...novedad,
+          imagen
+        }
+    } else{
+      return {
+        ...novedad,
+        imagen: ``
+      }
+    }
+  
+  });
+
+res.render('novedades',{novedades});
 });
+
+
 
 module.exports = router;
